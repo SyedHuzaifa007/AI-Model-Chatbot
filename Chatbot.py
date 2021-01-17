9b01e1c69e33bf364dc00961026b17353579cfc9
SyedHuzaifa007 <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-01-16 23:00:24 -0800

    Updated the file.

--- Chatbot.py
+++ Chatbot.py
@@ -4,9 +4,9 @@ import json
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tenserflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Embedding, GlobalAvergePooling1D
-from tenserflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 
@@ -46,3 +46,13 @@ word_index = tokenizer.word_index
 sequences = tokenizer.texts_to_sequences(training_sentences)
 padded_sequences = pad_sequences(sequences, truncating = 'post', maxlen = maximum_length)
 
+# Making the Neural Network Architecture
+
+model = Sequential()
+model.add(Embedding(vocab, embed_dim, input_length=maximum_length))
+model.add(GlobalAvergePooling1D)
+model.add(Dense(16,activation='rulu'))
+model.add(Dense(16,activation='relu'))
+model.add(Dense(num_classes, activation = 'softmax'))
+model.commpile(loss='sparse_categorical_crossentropy', optimizer = 'adam',metrics=['accuracy'])
+model.summary()
